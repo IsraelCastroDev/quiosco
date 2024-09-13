f69cac9e29ddc9dce6cc69b3f5f2e599c7987375
Isreal Castro <israelcastro.dev@gmail.com>
2024-09-13 18:17:28 -0500

    Calculando el total

--- components/Order/OrderSumary.tsx
+++ components/Order/OrderSumary.tsx
@@ -3,10 +3,16 @@
 import { useStore } from "@/src/store/store";
 import { useMemo } from "react";
 import ProductDetails from "./ProductDetails";
+import { formatCurrency } from "@/src/utils";
 
 function OrderSumary() {
   const order = useStore((state) => state.order);
   const orderIsEmpty = useMemo(() => order.length === 0, [order]);
+
+  const total = useMemo(
+    () => order.reduce((acc, item) => acc + item.subtotal, 0),
+    [order]
+  );
 
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
@@ -19,6 +25,10 @@ function OrderSumary() {
           {order.map((order) => (
             <ProductDetails key={order.id} item={order} />
           ))}
+
+          <p className="text-2xl font-bold mt-3 text-center">
+            Total: <span>{formatCurrency(total)}</span>
+          </p>
         </div>
       )}
     </aside>
